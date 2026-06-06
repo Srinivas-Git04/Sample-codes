edb3d159d8fc7c507961adef6940aeab96b9a536
Srinivas-Git04 <srinivas.boddupalli1101@gmail.com>
2026-06-06 13:02:21 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,20 @@
+n = int(input("Enter a number : "))
+
+for i in range (1,n+1):
+    print(i, end=" ")
+print()
+
+for i in range(2,n+1,2):
+    print(i, end=" ") 
+print()
+
+for i in range(1,n+1,2):
+    print(i, end= " ") 
+print()
+
+for i in range(1,n+1):
+    print(i*2,end=" ") 
+print()
+
+for i in range(1,n+1):
+    print(i*2-1,end=" ")
